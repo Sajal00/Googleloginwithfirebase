bcb8f2d233fc28cdd523dc48725ba905e64d65fe
Sajal00 <sajalbairagi9@gmail.com>
2023-08-26 21:15:50 +0530

    google signIn and signout

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,11 +1,103 @@
-import React from 'react';
-import {View, Text} from 'react-native';
+import React, {useState} from 'react';
+import {View, Image, Text, Dimensions, Button, StyleSheet} from 'react-native';
+import {
+  GoogleSignin,
+  statusCodes,
+} from '@react-native-google-signin/google-signin';
+
+const Devicewidth = Dimensions.get('window').width;
+const Deviceheight = Dimensions.get('window').height;
+
 const Home = () => {
+  const [loggedIn, setloggedIn] = useState(false);
+  const [userInfo, setuserInfo] = useState('');
+
+  // useEffect(() => {
+  //   configureGoogleSignIn(); // Call configure once when component mounts
+  // }, []);
+
+  const configureGoogleSignIn = async () => {
+    await GoogleSignin.configure({
+      scopes: ['email'],
+      // Scopes you want to request
+      androidClientId:
+        '767688653746-5rkk6i0bnknpfo9addk04dki5o7ed94m.apps.googleusercontent.com',
+      webClientId:
+        '767688653746-2kqoh7camjbq13ujffv6jntudtcf2f4d.apps.googleusercontent.com', // Replace with your web client ID
+      offlineAccess: true, // Allow offline access
+    });
+  };
+
+  const signIn = async () => {
+    try {
+      await configureGoogleSignIn(); // Call the configure function before sign-in
+      await GoogleSignin.hasPlayServices();
+      const {user} = await GoogleSignin.signIn();
+      setloggedIn(true);
+      setuserInfo(user);
+      console.log('google signin response', user);
+    } catch (error) {
+      console.log('Google login error', error);
+      if (error.code === statusCodes.SIGN_IN_CANCELLED) {
+        alert('Sign-in process canceled');
+      } else if (error.code === statusCodes.IN_PROGRESS) {
+        alert('Sign-in process already in progress');
+      } else if (error.code === statusCodes.PLAY_SERVICES_NOT_AVAILABLE) {
+        alert('Google Play services are not available');
+      } else {
+        alert('An error occurred during sign-in');
+      }
+    }
+  };
+
+  const signOut = async () => {
+    try {
+      await GoogleSignin.revokeAccess();
+      await GoogleSignin.signOut();
+      setloggedIn(false);
+      setuserInfo('');
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
-    <View>
-      <Text>Home</Text>
+    <View style={styles.mainview}>
+      <Text>Here we will show google login credential</Text>
+      {loggedIn ? (
+        <View
+          style={{
+            alignItems: 'center',
+            height: Deviceheight / 1.6,
+            justifyContent: 'space-evenly',
+          }}>
+          <View style={styles.profilephoto}>
+            <Image source={{uri: userInfo.photo}} style={styles.profilephoto} />
+          </View>
+          <Text>Welcome , {userInfo.name}</Text>
+          <Text>Email: {userInfo.email}</Text>
+          <Button title="Sign out " onPress={signOut}></Button>
+        </View>
+      ) : (
+        <Button title="Sign In with Google" onPress={signIn}></Button>
+      )}
     </View>
   );
 };
+const styles = StyleSheet.create({
+  mainview: {
+    height: Deviceheight,
+    width: Devicewidth,
+    backgroundColor: 'skyblue',
+    justifyContent: 'space-around',
+    alignItems: 'center',
+  },
+  profilephoto: {
+    height: Deviceheight / 3.7,
+    width: Devicewidth / 1.9,
+    backgroundColor: 'red',
+    borderRadius: 120,
+  },
+});
 
 export default Home;
--- src/screens/Splash.js
+++ src/screens/Splash.js
@@ -1,13 +1,24 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {View, Text, Dimensions, StyleSheet} from 'react-native';
 
 const Devicewidth = Dimensions.get('window').width;
 const Deviceheight = Dimensions.get('window').height;
 
-const Splash = () => {
+const Splash = props => {
+  useEffect(() => {
+    setTimeout(() => {
+      props.navigation.replace('homeScreen');
+    }, 1000);
+  }, []);
+
   return (
     <View style={styles.mainview}>
-      <View style={styles.Textcontainer}></View>
+      <View style={styles.Textcontainer}>
+        <Text>
+          This is splash screen After 1 second it will navigate home screen.
+        </Text>
+        <Text>There we will show google login</Text>
+      </View>
     </View>
   );
 };
@@ -21,7 +32,7 @@ const styles = StyleSheet.create({
   Textcontainer: {
     height: Deviceheight / 1.9,
     width: Devicewidth / 1.8,
-    backgroundColor: 'grey',
+    // backgroundColor: 'grey',
     alignSelf: 'center',
   },
 });
